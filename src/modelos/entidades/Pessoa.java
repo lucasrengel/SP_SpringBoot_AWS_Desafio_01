e21233af849d5dda95a4a378dc2f2e021d74531f
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-10-03 22:01:59 -0300

    feat: cria a classe pessoa

--- src/modelos/entidades/Pessoa.java
+++ src/modelos/entidades/Pessoa.java
@@ -1,4 +1,51 @@
 package modelos.entidades;
 
-public class Pessoa {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public Pessoa(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Nome: " + nome;
+    }
 }
